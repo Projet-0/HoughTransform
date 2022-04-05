173b55433c437698d6885b5873dee2d9230d367b
Projet-0 <98283760+Projet-0@users.noreply.github.com>
2022-04-05 09:43:36 +0200

    Add files via upload

--- Hough_Transform.py
+++ Hough_Transform.py
@@ -11,53 +11,37 @@ import skimage.io
 from skimage.color import rgb2gray
 import matplotlib.image as mpimg
 from scipy.ndimage import gaussian_filter
+from picamera import PiCamera
+from time import sleep
 
-
-#original_image = cv2.imread('C:/Users/Tanguy travail/Downloads/Pic1.jpeg',1)
-#gray_image = cv2.imread('Sample_Input.jpg',0)
-
-#output = original_image.copy()
-
-#Gaussian Blurring of Gray Image
-#blur_image = cv2.GaussianBlur(original_image,(3,3),0)
-
-#Using OpenCV Canny Edge detector to detect edges
-#edged_image = cv2.Canny(blur_image,75,150)
-#cv2.imshow('Edged Image', edged_image)
-
-#print("A")
+link = '/home/pi/Desktop/image0.jpg'
 
 start_time = time.time()
 
-# original_image = mpimg.imread('/home/pi/Documents/une-ping-pong.jpeg')
+k = 0
 
-original_image = mpimg.imread('C:/Users/Tanguy travail/Downloads/HoughTransform-main/HoughTransform-main/une-ping-pong.jpeg')
-R,G,B = original_image[:,:,0],original_image[:,:,1],original_image[:,:,2]
-ima = 0.2989*R + 0.5870*G + 0.1140*B
-blured = gaussian_filter(ima, sigma=1)
+# list1 = ['/home/pi/Desktop/image',k, '.jpg'] # Changement de type on le passe en liste pour implémenter le numéro de l'image
+# str1 = ''.join(list1)  #On repasse en format string pour stocker l'image dans la camera
+# print(str1)
 
-im = feature.canny(blured, sigma=1, low_threshold = 50, high_threshold = 150)
+camera = PiCamera() #on définit camera comme la caméra de la raspberry
+camera.resolution = (1024,768) #définir la résolution on va devoir rester en 16/9
 
 radi = 50
 pi = 3.14159
 
-Rmin = 50
+Rmin = 10
 Rmax = 150
-height,width,j = original_image.shape
 
-#cosl = np.zeros(radi)
-#sinl = np.zeros(radi)
-#for i in range(radi):
-#     cosl[i] = math.cos(2*pi*i/radi)
-#    sinl[i] = math.sin(2*pi*i/radi)
+pasR = 4
+pasXY = 4
 
+az = np.full((5,5),255) # Tableau blue de 5*5
 
 def point(arr,radi,x,y,Cmax): # Tranformation d'un point, arr = Accumulation; radi = Nombre d'angle; x,y = Coordonées
-    for r in range(Rmin,Rmax,4):
+    for r in range(Rmin,Rmax,pasR): # Pas
         for i in range(radi):
             teta = 2*pi*i/radi
-            #a = int(Rmax + x + r*cosl[i])
-            #b = int(Rmax + y + r*sinl[i])
             a = int(Rmax + x + r*math.cos(teta))
             b = int(Rmax + y + r*math.sin(teta))
             arr[a][b][r] += 1
@@ -66,95 +50,81 @@ def point(arr,radi,x,y,Cmax): # Tranformation d'un point, arr = Accumulation; ra
             # Variable global du max
     return Cmax
 
-# def max_2D(arr, height, width):
-#     max, x, y, z = 0, 0, 0, 0
-#     for i in range(2*Rmax + height):
-#         for j in range(2*Rmax + width):
-#             for k in range(Rmax):
-#                 if arr[i][j][k]>max:
-#                     x,y,z = i,j,k
-#                     max = arr[i][j][k]
-#                     print(max)
-#                 if arr[i][j][k]>= 100:
-#                     print(i,j,k,arr[i][j][k])
-#     return x,y,z,max,height,width
-
-def hough_transform(im): # Transformation d'un image filtre par canny
+def hough_transform(im):
     height,width = im.shape
     Cmax = [0,0,0,0] # a,b,r,valeur
     tot_array = np.zeros((height+2*Rmax,width+2*Rmax,Rmax))
     k = 0
-    for i in range(height):
-        for j in range(width):
+    for i in range(0,height,pasXY): # Pas
+        for j in range(0,width,pasXY): #Pas
             if im[i][j]!=0:
                 k +=1
                 Cmax = point(tot_array,radi,i,j,Cmax)
-    print(Cmax,k)
-    # return max_2D(tot_array, height, width, radi)
-
-def hough_transform2(im):
-    height,width = im.shape
-    Cmax = [0,0,0,0] # a,b,r,valeur
-    tot_array = np.zeros((height+2*Rmax,width+2*Rmax,Rmax))
-    k = 0
-    for i in range(0,height,4):
-        for j in range(0,width,4):
-            if im[i][j]!=0:
-                k +=1
-                Cmax = point(tot_array,radi,i,j,Cmax)
-    print(Cmax,k)
     return(Cmax,k)
 
-Cmax,k = hough_transform2(im)
-az = np.full((5,5),255)
-print(height, width)
-print(original_image[0:2][0:2][0])
-for i in range(Cmax[2]):
-    if (Cmax[0]-Rmax+i+2 < width) and (Cmax[0]-Rmax+i-2 >= 0) and (Cmax[1]-Rmax+2 < height) and (Cmax[1]-Rmax-2 >= 0):
-        print(Cmax[0]-Rmax+i+2)
-        original_image[Cmax[0]-Rmax+i-2:Cmax[0]-Rmax+i+2][Cmax[1]-Rmax-2:Cmax[1]-Rmax+2][0] = az
-    if (Cmax[0]-Rmax-i+2 < width) and (Cmax[0]-Rmax-i-2 >= 0) and (Cmax[1]-Rmax+2 < height) and (Cmax[1]-Rmax-2 >= 0):
-        original_image[Cmax[0]-Rmax-i-2:Cmax[0]-Rmax-i+2][Cmax[1]-Rmax-2:Cmax[1]-Rmax+2][0] = az
-    if (Cmax[0]-Rmax+2 < width) and (Cmax[0]-Rmax-2 >= 0) and (Cmax[1]-Rmax+i+2 < height) and (Cmax[1]-Rmax+i-2 >= 0):
-        original_image[Cmax[0]-Rmax-2:Cmax[0]-Rmax+2][Cmax[1]-Rmax+i-2:Cmax[1]-Rmax+i+2][0] = az
-    if (Cmax[0]-Rmax+2 < width) and (Cmax[0]-Rmax-2 >= 0) and (Cmax[1]-Rmax-i+2 < height) and (Cmax[1]-Rmax-i-2 >= 0):
-        print(Cmax[0]-Rmax-2)
-        print(Cmax[1]-Rmax-i-2)
-        original_image[Cmax[0]-Rmax-2:Cmax[0]-Rmax+2,Cmax[1]-Rmax-i-2:Cmax[1]-Rmax-i+2][0] = az
+def affichage(Cmax):
 
-for i in range(500):
-    teta = 2*pi*i/500
-    if int(Cmax[0]-Rmax+Cmax[2]*math.cos(teta))-2 >= 0 and int(Cmax[0]-Rmax+Cmax[2]*math.cos(teta))+2 < width and int(Cmax[1]-Rmax+Cmax[2]*math.sin(teta))-2 >= 0 and int(Cmax[1]-Rmax+Cmax[2]*math.sin(teta))+2 < height:
-        original_image[int(Cmax[0]-Rmax+Cmax[2]*math.cos(teta))-2:int(Cmax[0]-Rmax+Cmax[2]*math.cos(teta))+2][int(Cmax[1]-Rmax+Cmax[2]*math.sin(teta))-2:int(Cmax[1]-Rmax+Cmax[2]*math.sin(teta))+2][2] = 255
-#cv2.imshow('Identification', original_image)
+    for i in range(Cmax[2]):
+        if (Cmax[0]-Rmax+i+2 < width) and (Cmax[0]-Rmax+i-2 >= 0) and (Cmax[1]-Rmax+2 < height) and (Cmax[1]-Rmax-2 >= 0):
+            original_image[Cmax[0]-Rmax+i-2:Cmax[0]-Rmax+i+3,Cmax[1]-Rmax-2:Cmax[1]-Rmax+3,0] = az
+        if (Cmax[0]-Rmax-i+2 < width) and (Cmax[0]-Rmax-i-2 >= 0) and (Cmax[1]-Rmax+2 < height) and (Cmax[1]-Rmax-2 >= 0):
+            original_image[Cmax[0]-Rmax-i-2:Cmax[0]-Rmax-i+3,Cmax[1]-Rmax-2:Cmax[1]-Rmax+3,0] = az
+        if (Cmax[0]-Rmax+2 < width) and (Cmax[0]-Rmax-2 >= 0) and (Cmax[1]-Rmax+i+2 < height) and (Cmax[1]-Rmax+i-2 >= 0):
+            original_image[Cmax[0]-Rmax-2:Cmax[0]-Rmax+3,Cmax[1]-Rmax+i-2:Cmax[1]-Rmax+i+3,0] = az
+        if (Cmax[0]-Rmax+2 < width) and (Cmax[0]-Rmax-2 >= 0) and (Cmax[1]-Rmax-i+2 < height) and (Cmax[1]-Rmax-i-2 >= 0):
+            original_image[Cmax[0]-Rmax-2:Cmax[0]-Rmax+3,Cmax[1]-Rmax-i-2:Cmax[1]-Rmax-i+3,0] = az
+        
+    for i in range(500):
+        teta = 2*pi*i/500
+        original_image[int(Cmax[0]-Rmax+Cmax[2]*math.cos(teta))][int(Cmax[1]-Rmax+Cmax[2]*math.sin(teta))][0] = 255
+    #cv2.imshow('Identification', original_image)
 
 
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 3))
 
-ax[0].imshow(original_image)
-ax[0].set_title('Image originalle', fontsize=20)
+    ax[0].imshow(original_image)
+    ax[0].set_title('Image originalle', fontsize=20)
 
-ax[1].imshow(blured, cmap='gray')
-ax[1].set_title('Image après blur', fontsize=20)
+    ax[1].imshow(blured, cmap='gray')
+    ax[1].set_title('Image après blur', fontsize=20)
 
-ax[2].imshow(im, cmap='gray')
-ax[2].set_title('Canny', fontsize=20)
+    ax[2].imshow(im, cmap='gray')
+    ax[2].set_title('Canny', fontsize=20)
 
-for a in ax:
-    a.axis('off')
+    for a in ax:
+        a.axis('off')
 
-fig.tight_layout()
+    fig.tight_layout()
+    
+    plt.show()
+    
+# Fin boucle
 
 
+while True:
 
-#cv2.imshow('Edged Image', edged_image)
+    camera.start_preview()  #Lance la caméra
+
+    camera.capture(link) # A retester : récupération de l'image à partir du str1
+
+    camera.stop_preview() #Arrête l'affichage de la camér
+
+    original_image = mpimg.imread(link)
+    R,G,B = original_image[:,:,0],original_image[:,:,1],original_image[:,:,2] # Passage en noir et blanc
+    ima = 0.2989*R + 0.5870*G + 0.1140*B
+    blured = gaussian_filter(ima, sigma=1)
+
+    im = feature.canny(blured, sigma=1, low_threshold = 50, high_threshold = 150) # Sensibilité du filtre. A regler
+
+    height,width,j = original_image.shape # Taille de l'image
+
+    Cmax,k = hough_transform(im)
+    
+    print(Cmax[0]-Rmax, Cmax[1]-Rmax, Cmax[2])
+    
+    # affichage(Cmax)
+
 
 end_time = time.time()
 time_taken = end_time - start_time
 print ('Time taken for execution',time_taken)
-
-plt.show()
-
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
